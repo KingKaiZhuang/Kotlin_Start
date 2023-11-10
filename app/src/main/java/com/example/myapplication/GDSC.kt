cb2699672a49e4844b6ee734810e9e79a443dde1
STUST <5b0g0024@stust.edu.tw>
2023-11-10 17:21:39 +0800

    update

---
+++ app/src/main/java/com/example/myapplication/GDSC.kt
@@ -0,0 +1,64 @@
+package com.example.myapplication
+
+class GDSC {
+
+}
+
+fun main(){
+//    println("Hello world!")
+//    val text = "文字" // 不可變的變數
+//
+//    val number = 10
+//    var float = 10f // float
+//
+//    val isSunny = true // boolean
+//
+//    if(isSunny){
+//        print("今天是晴天")
+//    }else{
+//        print("今天下雨天")
+//    }
+
+//    var str = "hello\nworld"
+//    print(str)
+    // 強行別檢查
+//    var a:Int = 10
+//    var b:String = "100"
+    // 型別判斷
+//    val text:String = "文字"
+//    val output = text is String
+//    print(output)
+
+    // 身高體重練習
+//    val number = 10
+//
+//    val height = 100
+//    val heightText = height.toString()
+//    val weight = 70
+//
+//    val numberText = "身高:$height, 體重:$weight"
+//
+//    println(numberText)
+
+    // 運算元
+//    val a = 10
+//    val b = 20
+//
+//    val output1 = a.plus(b) // a + b
+//    val output2 = b.plus(a) // b + a
+//    println(output1)
+//    println(output2)
+
+    val a = 10
+    val b = 20
+
+    if(a == 20){
+        print("區塊1")
+    }else{
+        println("區塊2")
+    }
+
+}
+
+
+
